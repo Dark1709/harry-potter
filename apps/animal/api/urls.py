0ab39faaa50e1ff4b2321959cd1281227cb03f5a
dark1709 <darly.leon@alternova.com>
2023-07-26 18:37:38 -0500

    Corregido

--- apps/animal/api/urls.py
+++ apps/animal/api/urls.py
@@ -1,17 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register(
-    'api/v1/Animal',
-    views.AnimalModelViewSet,
-    basename='animal'
-)
 
 urlpatterns = [
     path('Animal', views.AnimalAPIView.as_view(), name='animal-list'),
-    path('Animal/<int:pk>', views.AnimalAPIView.as_view(), name='animal-detail'),
-    path('Animal', )
+    path('Animal/<int:pk>', views.AnimalAPIView.as_view(), name='animal-detail')
 ]
